63deaf048dd222d0847583f7a9ffc671cd698811
AmandaChou <AmandaChou@itsower.com.tw>
2021-11-01 15:21:09 +0800

    update poc

---
+++ implement/MPC/IStoreBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static implement.MPC.Media;
+
+namespace implement.MPC
+{
+    public interface IStoreBase<T, I>
+    {
+        T RawMedia { get; set; }
+        I ReportInfo { get; set; }
+
+    }
+
+    public class StoreBase<TMedia,TReport> : IStoreBase<TMedia, TReport>
+    {
+        public TMedia TMedia { get; set; }
+        public TReport TReport { get; set; }
+
+        public TReport ConverReport(TMedia rawMedia)
+        {
+            Attribute[] attrs = Attribute.GetCustomAttributes(rawMedia.GetType());
+            typeof(T).GetProperty("Name")
+
+            foreach (var attr in attrs)
+            {
+                switch attr.Name
+                    case 
+                //tofo 資料處理
+            }
+
+            return (TReport)Convert.ChangeType(new Report711(), typeof(T));
+        }
+    }
+
+
+}
---
+++ implement/MPC/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static implement.MPC.Media;
+
+namespace implement.MPC
+{
+    public class Store711<TMedia, TReport> : StoreBase<TMedia, TReport>
+    {
+        public Report711 ConverReport(Media711 Media711)
+        {
+            base.ConverReport(Media711);
+            return new Report711();
+        }
+
+    }
+
+    public class StoreFemaily<TMedia, TReport> : StoreBase<TMedia, TReport>
+    {
+
+    }
+}
