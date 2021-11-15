86af9d5f8ecdfe2ef670311a0e7df0f06923eee5
AmandaChou <AmandaChou@itsower.com.tw>
2021-11-15 16:11:46 +0800

    Add Proxy Controller

---
+++ ProxyPoc/ProxyHost/Controllers/ProxyHostController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProxyHost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProxyHostController : Controller
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return new
+            {
+                Data = "ProxyHost"
+            };
+        }
+    }
+}
