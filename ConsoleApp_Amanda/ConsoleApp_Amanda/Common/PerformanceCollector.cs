95c83d934e00840e99217dbb9f64f01983f8bfe9
AmandaChou <AmandaChou@itsower.com.tw>
2022-02-24 12:29:56 +0800

    update net core day1

---
+++ ConsoleApp_Amanda/ConsoleApp_Amanda/Common/PerformanceCollector.cs
@@ -0,0 +1,18 @@
+﻿namespace Common
+{
+    public class PerformanceCollector : IPerformanceCollector
+    {
+        public int GetCpuUsage()
+        {
+            return new Random().Next(0, 100);
+        }
+        public int GetMemoryUsage()
+        {
+            return new Random().Next(0, 99999);
+        }
+        public int GetMemoryUsage(string processName)
+        {
+            return new Random().Next(0, 99999);
+        }
+    }
+}
