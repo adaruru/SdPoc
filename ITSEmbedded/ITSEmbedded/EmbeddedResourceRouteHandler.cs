7d48948907cbcc971bdd06591434af4293d43046
AmandaChou <AmandaChou@itsower.com.tw>
2022-03-09 23:35:04 +0800

    add embedded test

---
+++ ITSEmbedded/ITSEmbedded/EmbeddedResourceRouteHandler.cs
@@ -0,0 +1,18 @@
+﻿using ITSEmbedded;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Routing;
+
+namespace ITSEmbedded
+{
+    public class EmbeddedResourceRouteHandler : IRouteHandler
+    {
+        IHttpHandler IRouteHandler.GetHttpHandler(RequestContext requestContext)
+        {
+            return new EmbeddedResourceHttpHandler(requestContext.RouteData);
+        }
+    }
+}
---
+++ ITSEmbedded/ITSEmbedded/Util/ContentUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Optimization;
+
+namespace ITSEmbedded.Util
+{
+    public static class ContentUtil
+    {
+        public static MvcHtmlString NewTextBox(this HtmlHelper html, string name)
+        {
+            var js = Scripts.Render("~/Scripts/bootstrap-datepicker/js/bootstrap-datepicker.js").ToString();
+            var css = Styles.Render("~/ImranB/Embedded/Css").ToString();
+            var textbox = html.TextBox(name).ToString();
+            return MvcHtmlString.Create(textbox + js + css);
+
+        }
+
+    }
+
+}
